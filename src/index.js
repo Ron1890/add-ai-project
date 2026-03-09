275dbca66a720cb325037f346664b32d8c9c92c7
Ron1890 <vero.diss90@gmail.com>
2026-03-09 20:27:56 +0000

    api integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-function generateFact(event){
-    event.preventDefault();
-    
-new Typewriter("#fact", {
-  strings: "The first literal computer bug was a real moth found stuck in a Harvard Mark II computer in 1947 by Grace Hopper's team",
-  autoStart: true,
-  delay:1,
-});
+function displayFact(response) {
+  new Typewriter("#fact", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+  });
 }
-let factsFormElement = document.querySelector("facts-generator-form");
-factsFormElement.addEventListener("submit",generateFact);
+
+function generateFact(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#userInstructions");
+
+  let apiKey = "137tbd11fb6e495a5570dofc60ef610d";
+
+  let prompt = `User instructions or topic: generate a fun fact about ${instructionsInput.value}`;
+
+  let content =
+    "You are a very funny AI who likes to tell short fun facts. Generate a fun fact in HTML and separate each line with <br/>. Make sure the fact reflects the user's topic.";
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${content}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayFact);
+}
+
+let factsFormElement = document.querySelector("#facts-generator-form");
+factsFormElement.addEventListener("submit", generateFact);
