003aafed334cf0c6f0594dce2df698ac5d51d7e4
Ron1890 <vero.diss90@gmail.com>
2026-03-24 09:18:20 +0000

    polishing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayFact(response) {
-  
+  let factElement = document.querySelector("#fact");
 
   new Typewriter(factElement, {
     strings: response.data.answer,
@@ -8,26 +8,25 @@ function displayFact(response) {
     cursor: "",
   });
 }
-
 function generateFact(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
- 
+
   let apiKey = "137tbd11fb6e495a5570dofc60ef610d";
 
   let prompt = `Generate a fun fact about ${instructionsInput.value}`;
   let context =
-    "You are a funny AI that writes one short fun fact in HTML. Separate lines with <br /> if needed.";
+    "You are a funny AI that writes one short fun fact in plain text. Separate lines with <br /> if needed.";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
     prompt,
   )}&context=${context}&key=${apiKey}`;
 
-  let factElement= document.querySelector("#fact");
+  let factElement = document.querySelector("#fact");
   factElement.classList.remove("hidden");
 
- factElement.innerHTML = `<div class= generating>Generating a fun fact about ${instructionsInput.value}`;
+  factElement.innerHTML = `<div class= generating>Generating a fun fact about ${instructionsInput.value}`;
   axios.get(apiURL).then(displayFact);
 }
 
